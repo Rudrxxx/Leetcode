24b6cd470e6f45488769b913c84fd07195d3cc1f
Rudransh <message.rudransh@gmail.com>
2025-05-18 22:21:34 +0530

    Time: 482 ms (48.89%), Space: 18 MB (71.11%) - LeetHub

--- 1931-painting-a-grid-with-three-different-colors/1931-painting-a-grid-with-three-different-colors.py
+++ 1931-painting-a-grid-with-three-different-colors/1931-painting-a-grid-with-three-different-colors.py
@@ -6,7 +6,6 @@ class Solution:
                 if st[i]==st[i-1]:
                     return False
             return True
-            
         colors=[0,1,2]
         validst=[]
         for i in product(colors,repeat=m):
