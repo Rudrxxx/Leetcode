4a43dba9b313f62083536041949f10800b4d393d
Rudransh <message.rudransh@gmail.com>
2026-01-23 23:10:48 +0530

    Time: 2285 ms (76.64%), Space: 56.3 MB (52.34%) - LeetHub

--- 3510-minimum-pair-removal-to-sort-array-ii/3510-minimum-pair-removal-to-sort-array-ii.py
+++ 3510-minimum-pair-removal-to-sort-array-ii/3510-minimum-pair-removal-to-sort-array-ii.py
@@ -1,50 +1,30 @@
 class Solution:
-    def minimumPairRemoval(self, nums: List[int]) -> int:
-        n = len(nums)
-        if n < 2:
-            return 0
-        idx = {0: [-1, 1]}
-        h = [(nums[0] + nums[1], 0)]
-        v = [h[0][0]]
-        for i in range(1, n - 1):
-            idx[i] = [i-1, i+1]
-            v.append(nums[i] + nums[i+1])
-            h.append((nums[i] + nums[i+1], i))
-        idx[n - 1] = [n-2, -1]
+    def minimumPairRemoval(self, l: List[int]) -> int:
+        n = len(l)
+        l.append(inf)
+        le, ri = list(range(-1, n)), list(range(1, n + 1))
+        h = [(a + b, i) for i, (a, b) in enumerate(pairwise(l))]
         heapify(h)
-        ans = 0
-        inversions = sum(1 for i in range(n-1) if nums[i] > nums[i+1])
-        while inversions > 0:
-            while h and (h[0][1] not in idx or h[0][0] != v[h[0][1]]):
-                val, i = heappop(h)
-                if i in idx and idx[i][1] >= 0:
-                    heappush(h, (v[i], i))
-            val, i = heappop(h)
-            prev, next = idx[i]
-            if next >= 0:
-                next_next = idx[next][1]
-                if nums[i] > nums[next]:
-                    inversions -= 1
-                idx[i][1] = next_next
-                if next_next >= 0:
-                    if nums[next] > nums[next_next]:
-                        inversions -= 1
-                    idx[next_next][0] = i 
-                if prev >= 0:
-                    if nums[prev] > nums[i]:
-                        inversions -= 1
-                    idx[prev][1] = i
-                idx.pop(next)
-                nums[i] = val
-                if idx[i][1] >= 0:
-                    v[i] = nums[i] + nums[next_next]
-                    heappush(h, (v[i], i))
-                    if nums[i] > nums[next_next]:
-                        inversions += 1
-                if prev >= 0:
-                    v[prev] = nums[prev] + val
-                    if nums[prev] > nums[i]:
-                        inversions += 1
-                    heappush(h, (v[prev], prev))
-                ans += 1
+
+        ans = 0 
+        rest = n - sum(1 for a, b in pairwise(l) if a <= b) 
+
+        while rest > 0:
+            v, i = heappop(h)
+            r = ri[i]
+            if le[r] != i or l[i] + l[r] != v:
+                continue
+
+            rr = ri[r]
+            rest += (l[le[i]] <= l[i]) + (l[i] <= l[r]) + (l[r] <= l[rr])
+            le[rr], ri[i] = i, rr
+            l[i] = v
+            rest -= 1 + (l[le[i]] <= l[i]) + (l[i] <= l[rr])
+            if i:
+                heappush(h, (l[le[i]] + l[i], le[i]))
+            if rr < n:
+                heappush(h, (l[i] + l[rr], i))
+
+            ans += 1
+        
         return ans
